c425cad1e059b6ce9319c9265316cff6e6c9a33b
Nursultan <nurrs.serkul@gmail.com>
2024-04-17 11:24:09 +0500

    results added to exam model

--- models/Exams.js
+++ models/Exams.js
@@ -5,6 +5,7 @@ const ExamsSchema = new Schema({
   topics: [{ type: mongoose.Schema.Types.ObjectId, ref: "Topics" }],
   status: { type: String, enum: ["active", "inactive"], default: "active" },
   startedAt: { type: Date },
+  results: [{ type: mongoose.Schema.Types.ObjectId, ref: "Results" }],
   finishedAt: { type: Date },
   examType: { type: String, enum: ["last", "random"], default: "random" },
   amountOfPassed: { type: Number, default: 0 },
