1afde2a15e437c3ff83adaf1cb2b7315a93379a5
Nursultan <nurrs.serkul@gmail.com>
2024-04-18 05:28:11 +0500

    teacher can manipulate with students

--- routes/teachers.js
+++ routes/teachers.js
@@ -2,111 +2,13 @@ const router = require("express").Router();
 const _ = require("lodash");
 
 const Teachers = require("../models/Teachers");
+const Classes = require("../models/Classes");
 const ApiOptimizer = require("../api");
 const errorHandler = require("../middleware/errorHandler");
 
 const teacher = new ApiOptimizer(Teachers);
 const modelName = "Teacher";
 
-/**
- * @swagger
- * tags:
- *   - name: Teacher
- *     description: Operations on Teachers.
- *
- * /teachers/:
- *   get:
- *     summary: Get all teachers
- *     tags: [Teacher]
- *     responses:
- *       200:
- *         description: A list of all teachers.
- *         content:
- *           application/json:
- *             examples:
- *               allTeachers:
- *                 summary: Example response of all teachers
- *                 value:
- *                   - name: John Doe
- *                     email: johndoe@example.com
- *                     classes: ['Math', 'Science']
- *                     literal: '10A'
- *                   - name: Jane Smith
- *                     email: janesmith@example.com
- *                     classes: ['History', 'English']
- *                     literal: '10B'
- *
- * /teachers/{id}:
- *   get:
- *     summary: Get a teacher by ID
- *     tags: [Teacher]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Details of a specific teacher.
- *         content:
- *           application/json:
- *             examples:
- *               specificTeacher:
- *                 summary: Example response for a specific teacher
- *                 value:
- *                   name: John Doe
- *                   email: johndoe@example.com
- *                   classes: ['Math', 'Science']
- *                   literal: '10A'
- *       404:
- *         description: Teacher not found
- *
- *   delete:
- *     summary: Delete a teacher by ID
- *     tags: [Teacher]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Teacher deleted successfully.
- *       404:
- *         description: Teacher not found
- *
- *   put:
- *     summary: Update a teacher by ID
- *     tags: [Teacher]
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           examples:
- *             updateTeacher:
- *               summary: Example of updating a teacher
- *               value:
- *                 name: John Doe Updated
- *                 email: johndoeupdated@example.com
- *                 password: newPassword123
- *                 classes: ['Mathematics', 'Science']
- *                 literal: '10A'
- *     responses:
- *       200:
- *         description: Teacher updated successfully.
- *       404:
- *         description: Teacher not found
- */
-
-// get all done
 router.route("/").get(async (req, res) => {
   try {
     await teacher.getAll(req, res);
@@ -115,7 +17,6 @@ router.route("/").get(async (req, res) => {
   }
 });
 
-//delete an teacher by id done
 router.route("/:id").delete(async (req, res) => {
   try {
     await teacher.deleteById(req, res, modelName);
@@ -124,7 +25,6 @@ router.route("/:id").delete(async (req, res) => {
   }
 });
 
-// get by id done
 router.route("/:id").get(async (req, res) => {
   try {
     await teacher.getById(req, res, modelName);
@@ -133,13 +33,46 @@ router.route("/:id").get(async (req, res) => {
   }
 });
 
-// Update teacher done
 router.route("/:id").put(async (req, res) => {
   try {
     const entityId = _.get(req, "params.id");
     const { name, email, password, classes, literal } = req.body;
     const fieldsToUpdate = { name, email, password, classes, literal };
     await teacher.updateById({ entityId, fieldsToUpdate, req, res });
+  } catch (err) {
+    errorHandler(err, req, res);
+  }
+});
+
+router.get("/class/:classId", async (req, res) => {
+  try {
+    const classId = req.params.classId;
+
+    // Find the class by its ID
+    const classWithStudents = await Classes.findById(classId).populate({
+      path: "students",
+      populate: {
+        path: "user",
+        model: "Users",
+      },
+    });
+
+    if (!classWithStudents) {
+      return res.status(404).send("Class not found.");
+    }
+
+    // Extract student data from the populated class document
+    const students = classWithStudents.students.map((student) => {
+      return {
+        id: student._id,
+        name: student.user.name,
+        surname: student.user.surname,
+        email: student.user.email,
+        inn: student.inn,
+      };
+    });
+
+    res.status(200).json(students);
   } catch (err) {
     errorHandler(err, req, res);
   }
