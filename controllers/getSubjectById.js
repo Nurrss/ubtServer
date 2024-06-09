6c9a5b2830f8c2e4bec7112ca708b7452239f35c
Nursultan <nurrs.serkul@gmail.com>
2024-06-10 04:51:21 +0500

    updated

--- controllers/getSubjectById.js
+++ controllers/getSubjectById.js
@@ -1,13 +1,98 @@
 const Subjects = require("../models/Subjects");
-const {
-  getTopicsWithQuestionCount,
-} = require("../controllers/GetQuestionIdsByPoints");
+const mongoose = require("mongoose");
+const Topic = require("../models/Topics");
+
+async function getTopicsWithQuestionCount(topicIds) {
+  return Topic.aggregate([
+    {
+      $match: {
+        _id: { $in: topicIds.map((id) => new mongoose.Types.ObjectId(id)) },
+      },
+    },
+    {
+      $lookup: {
+        from: "questions",
+        localField: "ru_questions",
+        foreignField: "_id",
+        as: "ruQuestionsInfo",
+      },
+    },
+    {
+      $lookup: {
+        from: "questions",
+        localField: "kz_questions",
+        foreignField: "_id",
+        as: "kzQuestionsInfo",
+      },
+    },
+    {
+      $project: {
+        kz_title: 1,
+        ru_title: 1,
+        ruQuestionsInfo: 1,
+        kzQuestionsInfo: 1,
+        ru_twoPointsQuestionIds: {
+          $map: {
+            input: {
+              $filter: {
+                input: "$ruQuestionsInfo",
+                as: "question",
+                cond: { $eq: ["$$question.type", "twoPoints"] },
+              },
+            },
+            as: "question",
+            in: "$$question._id",
+          },
+        },
+        ru_onePointQuestionIds: {
+          $map: {
+            input: {
+              $filter: {
+                input: "$ruQuestionsInfo",
+                as: "question",
+                cond: { $eq: ["$$question.type", "onePoint"] },
+              },
+            },
+            as: "question",
+            in: "$$question._id",
+          },
+        },
+        kz_twoPointsQuestionIds: {
+          $map: {
+            input: {
+              $filter: {
+                input: "$kzQuestionsInfo",
+                as: "question",
+                cond: { $eq: ["$$question.type", "twoPoints"] },
+              },
+            },
+            as: "question",
+            in: "$$question._id",
+          },
+        },
+        kz_onePointQuestionIds: {
+          $map: {
+            input: {
+              $filter: {
+                input: "$kzQuestionsInfo",
+                as: "question",
+                cond: { $eq: ["$$question.type", "onePoint"] },
+              },
+            },
+            as: "question",
+            in: "$$question._id",
+          },
+        },
+      },
+    },
+  ]);
+}
 
 async function getSubjectById(req, res) {
   try {
     const subject = await Subjects.findById(req.params.id).populate({
       path: "topics",
-      select: "_id",
+      select: "_id kz_title ru_title",
     });
 
     if (!subject) {
