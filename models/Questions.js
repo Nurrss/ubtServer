2cf1d8bae76b76bae774d61da3e319ce4f454a3b
Nursultan <nurrs.serkul@gmail.com>
2024-07-26 16:29:26 +0500

    updated quesiton schema

--- models/Questions.js
+++ models/Questions.js
@@ -2,34 +2,37 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const Options = require("./Options");
 
-const QuestionsSchema = new Schema({
-  question: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  options: [{ type: mongoose.Schema.Types.ObjectId, ref: "Options" }],
-  point: { type: Number },
-  type: {
-    type: String,
-    enum: ["twoPoints", "onePoint"],
-    default: "onePoint",
-  },
-  correctOptions: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Options",
+const QuestionsSchema = new Schema(
+  {
+    question: {
+      type: String,
       required: true,
     },
-  ],
-  language: {
-    type: String,
-    required: true,
-    enum: ["kz", "ru"],
+    image: {
+      type: String,
+    },
+    options: [{ type: mongoose.Schema.Types.ObjectId, ref: "Options" }],
+    point: { type: Number },
+    type: {
+      type: String,
+      enum: ["twoPoints", "onePoint"],
+      default: "onePoint",
+    },
+    correctOptions: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Options",
+        required: true,
+      },
+    ],
+    language: {
+      type: String,
+      required: true,
+      enum: ["kz", "ru"],
+    },
   },
-});
+  { timestamps: true }
+);
 
 QuestionsSchema.pre(
   "deleteOne",
